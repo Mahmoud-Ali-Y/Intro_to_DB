079336f5f255ecb359de87dbd68faa6f55df0a6b
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-10-24 09:39:29 +0300

    Second Commit

---
+++ MySQLServer.py
@@ -0,0 +1,16 @@
+import mysql.connector
+try:
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "MHD963852147a#")
+    if mydb.is_connected():
+        cursor = mydb.cursor()
+        cursor.execute("CREATE DATABASE alx_book_store")
+        print("Database alx_book_store created successfully.")
+except Error as e:
+    print(f"Error occured: {e}")
+finally:
+    if mydb.is_connected:
+        cursor.close()
+        mydb.close()
